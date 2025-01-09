894816ad7dcd85a0e2e24b3cc99930409b89f42e
Felipe Santos <felipe.santos@qualyteam.com.br>
2025-01-09 11:18:02 -0300

    Add: Create Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './global.css'
+
+import { RouterProvider } from 'react-router-dom'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+
+import { router } from './routes'
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
+}
