ab733b129bfb5e8d536f4d030dc46f36eb3123bd
Felipe Santos <felipe.santos@qualyteam.com.br>
2025-02-01 23:14:52 -0300

    Add: Get Orders from Api

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -3,8 +3,21 @@ import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { ArrowRight, Search, X } from "lucide-react";
 import { OrderDetails } from "./order-details";
+import { OrderStatus } from "@/components/order-status";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
-export function OrderTableRow() {
+export interface OrderTableRowProps {
+    order: {
+        orderId: string;
+        createdAt: string;
+        status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+        customerName: string;
+        total: number;
+    }
+}
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
     return (
         <TableRow>
             <TableCell>
@@ -20,22 +33,29 @@ export function OrderTableRow() {
                 </Dialog>
             </TableCell>
             <TableCell className="font-mono text-xs font-medium">
-                e38e7hf37hf73hfe
+                {order.orderId}
             </TableCell>
             <TableCell className="text-muted-foreground">
-                há 15 minutos
+                {
+                    formatDistanceToNow(order.createdAt, {
+                        locale: ptBR,
+                        addSuffix: true,
+                    })
+                }
             </TableCell>
             <TableCell>
-                <div className="flex items-center gap-2">
-                    <span className="h-2 w-2 rounded-full bg-slate-400" />
-                    <span className="font-medium text-muted-foreground">Pendente</span>
-                </div>
+                <OrderStatus status={order.status} />
             </TableCell>
             <TableCell className="font-medium">
-                Felipe Kimura
+                {order.customerName}
             </TableCell>
             <TableCell className="font-medium">
-                R$ 149,90
+                {
+                    order.total.toLocaleString("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                    })
+                }
             </TableCell>
             <TableCell>
                 <Button variant="outline" size="xs">
