ca41f6d12ccf0d92415f806ee4d5555f77b54718
Felipe Santos <felipe.santos@qualyteam.com.br>
2025-02-19 10:20:51 -0300

    Add: Dashboard Mock Backend Request

---
+++ src/api/mocks/get-month-revenue-mock.ts
@@ -0,0 +1,9 @@
+import { http, HttpResponse } from "msw";
+import { GetMonthRevenueResponse } from "../get-month-renevue";
+
+export const getMonthRevenuetMock = http.get<never, never, GetMonthRevenueResponse>('/metrics/month-receipt', () => {
+    return HttpResponse.json({
+        receipt: 20000,
+        diffFromLastMonth: 10,
+    })
+});
