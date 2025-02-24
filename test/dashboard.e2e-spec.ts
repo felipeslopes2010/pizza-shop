f8cd9c178adf40a87feb9675e889d5ec9cf2350f
Felipe Santos <felipe.santos@qualyteam.com.br>
2025-02-24 13:40:05 -0300

    Add: Order Page E2E Test

--- test/dashboard.e2e-spec.ts
+++ test/dashboard.e2e-spec.ts
@@ -3,31 +3,31 @@ import { test, expect } from '@playwright/test';
 test('display month revenue metric', async ({ page }) => {
     await page.goto('/', { waitUntil: 'networkidle' });
 
-    expect(page.getByText('Receita total (mês)')).toBeVisible();
-    expect(page.getByText('R$ 200,00')).toBeVisible();
-    expect(page.getByText('+10% em relação ao mês passado')).toBeVisible();
+    await expect(page.getByText('Receita total (mês)')).toBeVisible();
+    await expect(page.getByText('R$ 200,00')).toBeVisible();
+    await expect(page.getByText('+10% em relação ao mês passado')).toBeVisible();
 });
 
 test('display month orders amount metric', async ({ page }) => {
     await page.goto('/', { waitUntil: 'networkidle' });
 
-    expect(page.getByText('Pedidos (mês)')).toBeVisible();
-    expect(page.getByText('200', { exact: true })).toBeVisible();
-    expect(page.getByText('+7% em relação ao mês passado')).toBeVisible();
+    await expect(page.getByText('Pedidos (mês)')).toBeVisible();
+    await expect(page.getByText('200', { exact: true })).toBeVisible();
+    await expect(page.getByText('+7% em relação ao mês passado')).toBeVisible();
 });
 
 test('display day orders amount metric', async ({ page }) => {
     await page.goto('/', { waitUntil: 'networkidle' });
 
-    expect(page.getByText('Pedidos (dia)')).toBeVisible();
-    expect(page.getByText('20', { exact: true })).toBeVisible();
-    expect(page.getByText('-5% em relação a ontem')).toBeVisible();
+    await expect(page.getByText('Pedidos (dia)')).toBeVisible();
+    await expect(page.getByText('20', { exact: true })).toBeVisible();
+    await expect(page.getByText('-5% em relação a ontem')).toBeVisible();
 });
 
 test('display month canceled orders amount metric', async ({ page }) => {
     await page.goto('/', { waitUntil: 'networkidle' });
 
-    expect(page.getByText('Cancelamentos (mês)')).toBeVisible();
-    expect(page.getByText('5', { exact: true })).toBeVisible();
-    expect(page.getByText('-5% em relação ao mês passado')).toBeVisible();
+    await expect(page.getByText('Cancelamentos (mês)')).toBeVisible();
+    await expect(page.getByText('5', { exact: true })).toBeVisible();
+    await expect(page.getByText('-5% em relação ao mês passado')).toBeVisible();
 });
